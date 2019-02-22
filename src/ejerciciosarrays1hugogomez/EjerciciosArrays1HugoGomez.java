4827fdc8ee73cb5f65b583b216dee500ae8658ec
Chocita <Chocita@DESKTOP-TOV6SFL>
2019-02-22 20:48:17 +0100

    Primera version

--- src/ejerciciosarrays1hugogomez/EjerciciosArrays1HugoGomez.java
+++ src/ejerciciosarrays1hugogomez/EjerciciosArrays1HugoGomez.java
@@ -7,13 +7,10 @@ package ejerciciosarrays1hugogomez;
 
 /**
  *
- * @author Chocita
+ * @author Hugo Gomez Amador
  */
 public class EjerciciosArrays1HugoGomez {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
